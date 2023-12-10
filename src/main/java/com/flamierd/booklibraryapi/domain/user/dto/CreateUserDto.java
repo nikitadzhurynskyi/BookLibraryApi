09caf2d3a6a95e71320e5f99690705787e23e888
Flamierdx <nikitadzhurinskiy06@gmail.com>
2023-12-10 19:29:21 +0000

    User role service and repository created, changed create user method

--- src/main/java/com/flamierd/booklibraryapi/domain/user/dto/CreateUserDto.java
+++ src/main/java/com/flamierd/booklibraryapi/domain/user/dto/CreateUserDto.java
@@ -1,17 +1,13 @@
 package com.flamierd.booklibraryapi.domain.user.dto;
 
 import com.flamierd.booklibraryapi.domain.user.model.UserRole;
-import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.Builder;
 
 import java.io.Serializable;
 import java.util.Set;
 
-/**
- * DTO for {@link com.flamierd.booklibraryapi.domain.user.model.User}
- */
-@EqualsAndHashCode(callSuper = true)
-@Value
+
+@Builder
 public record CreateUserDto(String email,
                             String password,
                             Set<UserRole> authorities) implements Serializable {
--- src/main/java/com/flamierd/booklibraryapi/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/flamierd/booklibraryapi/domain/user/service/impl/UserServiceImpl.java
@@ -3,17 +3,22 @@ package com.flamierd.booklibraryapi.domain.user.service.impl;
 import com.flamierd.booklibraryapi.core.exception.NotFoundException;
 import com.flamierd.booklibraryapi.domain.user.dto.CreateUserDto;
 import com.flamierd.booklibraryapi.domain.user.model.User;
+import com.flamierd.booklibraryapi.domain.user.model.UserRole;
 import com.flamierd.booklibraryapi.domain.user.repository.UserRepository;
+import com.flamierd.booklibraryapi.domain.user.service.UserRoleService;
 import com.flamierd.booklibraryapi.domain.user.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 
 @AllArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
+    private final UserRoleService userRoleService;
 
     @Override
     public Optional<User> findByEmail(String email) {
@@ -28,10 +33,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(CreateUserDto dto) {
+        Set<UserRole> authorities = dto.authorities() != null ? dto.authorities() : userRoleService.getDefaultUserRoles();
         User user = User.builder()
                 .email(dto.email())
                 .password(dto.password())
-                .authorities(dto.authorities())
+                .authorities(authorities)
                 .build();
         return userRepository.save(user);
     }
